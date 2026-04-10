3391248af4b0eb14c0660b332801148d182ea298
iTitus <iTitus@users.noreply.github.com>
2026-04-10 10:20:09 +0200

    mark Interrupt functions as const

--- src/interrupt.rs
+++ src/interrupt.rs
@@ -5,36 +5,36 @@ pub struct Interrupt {
 }
 
 impl Interrupt {
-    pub fn none() -> Self {
+    pub const fn none() -> Self {
         Self {
             irq: false,
             nmi: false,
         }
     }
 
-    pub fn irq() -> Self {
+    pub const fn irq() -> Self {
         Self {
             irq: true,
             nmi: false,
         }
     }
 
-    pub fn nmi() -> Self {
+    pub const fn nmi() -> Self {
         Self {
             irq: false,
             nmi: true,
         }
     }
 
-    pub fn is_irq(&self) -> bool {
+    pub const fn is_irq(&self) -> bool {
         self.irq
     }
 
-    pub fn is_nmi(&self) -> bool {
+    pub const fn is_nmi(&self) -> bool {
         self.nmi
     }
 
-    pub fn or(self, other: Self) -> Self {
+    pub const fn or(self, other: Self) -> Self {
         Self {
             irq: self.irq | other.irq,
             nmi: self.nmi | other.nmi,
